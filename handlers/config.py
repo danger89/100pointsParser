1abd203c3f1d89be05db18986fcca86e11d4e679
slmv-e <fiddler-mush0i@icloud.com>
2022-11-03 23:48:24 +0300

    bug fixes

--- handlers/config.py
+++ handlers/config.py
@@ -7,11 +7,11 @@ class Config:
         self.param = param
 
     def get(self):
-        with open(self.path, encoding="utf-8") as input_file:
+        with open(self.path) as input_file:
             return json.load(input_file)[self.param]
 
     def add(self, arg):
-        with open(self.path, encoding="utf-8") as input_file:
+        with open(self.path) as input_file:
             data = json.load(input_file)
 
         if self.param == "courses_list":
@@ -20,5 +20,5 @@ class Config:
         elif self.param == "last_homework_info":
             data[self.param] = arg
 
-        with open(self.path, "w", encoding="utf-8") as output_file:
+        with open(self.path, "w") as output_file:
             json.dump(data, output_file, ensure_ascii=False, indent=4)
